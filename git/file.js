394b458c8b7b6f6355a0530e447a512c31c36529
BASIM THANI <mjnoonha90@gmail.com>
2025-12-18 00:30:46 +0300

    feat: add CI pipeline

--- git/file.js
+++
@@ -1,2 +0,0 @@
-console.log('hello, git!')
-console.log('jest test!')
---
+++ git/test.js
@@ -0,0 +1,5 @@
+console.log('Starting test...')
+setTimeout(() => {
+  console.log('Test is running ')
+}, 5000);
+console.log('Test End')
