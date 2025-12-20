5cc1ee5cedf6bb75db916d5852f4a8a0f1c0f9f9
BASIM THANI <mjnoonha90@gmail.com>
2025-12-20 18:25:09 +0300

    file structure change

--- DevOps_demo/index.js
+++
@@ -1,2 +0,0 @@
-console.log('hello, git!')
-console.log('Learing CI/CD !')
--- DevOps_demo/test.js
+++
@@ -1,5 +0,0 @@
-console.log('Starting test')
-setTimeout(() => {
-  console.log('Test is running... ')
-}, 5000);
-console.log('Test End')
